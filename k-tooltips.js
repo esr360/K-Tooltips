ca5fb9c99881a85f401ec2faf658ce72c61ec71e
Edmund <esr360@live.com>
2015-07-09 23:24:33 +0100

    updating mobile support

--- k-tooltips.js
+++ k-tooltips.js
@@ -21,7 +21,7 @@
 				var $position = options.position;		
 				var $style = options.style;
 				
-				$(this).attr("ontouchstart", "");
+				$(this).attr("ontouchstart", ""); 
 				
 				$(this).append(
 					$("<div class='tooltip-wrapper-" + $position + "-" + $style + "'><div class='tooltip-content'>"+ $content +"</div></div>")
